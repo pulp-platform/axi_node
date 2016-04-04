63ae6166ca201cc68d5ca46a95625eae0c7ac287
Olof Kindgren <olof.kindgren@gmail.com>
2016-04-04 22:09:24 +0200

    Syntax fix for verilator

--- axi_node_wrap.sv
+++ axi_node_wrap.sv
@@ -68,8 +68,8 @@ module axi_node_wrap
     input logic                                                          rst_n,
 
     //MASTER PORTS
-    AXI_BUS.Slave                                                        axi_port_slave  [N_SLAVE_PORT],
-    AXI_BUS.Master                                                       axi_port_master [N_MASTER_PORT],
+    AXI_BUS.Slave                                                        axi_port_slave  [N_SLAVE_PORT:N_SLAVE_PORT],
+    AXI_BUS.Master                                                       axi_port_master [N_MASTER_PORT:N_MASTER_PORT],
 
     `ifdef USE_CFG_BLOCK
         `ifdef USE_AXI_LITE
