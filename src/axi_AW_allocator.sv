f943872c9b00d8cbf4a5099ae06eaf7cec0976b3
Andreas Kurth <akurth@iis.ee.ethz.ch>
2018-12-04 09:41:22 +0100

    axi_AW_allocator: :art: Refactor, clean, and format

--- src/axi_AW_allocator.sv
+++ src/axi_AW_allocator.sv
@@ -8,144 +8,126 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-// ============================================================================= //
-// Company:        Multitherman Laboratory @ DEIS - University of Bologna        //
-//                    Viale Risorgimento 2 40136                                 //
-//                    Bologna - fax 0512093785 -                                 //
-//                                                                               //
-// Engineer:       Igor Loi - igor.loi@unibo.it                                  //
-//                                                                               //
-//                                                                               //
-// Additional contributions by:                                                  //
-//                                                                               //
-//                                                                               //
-//                                                                               //
-// Create Date:    01/02/2014                                                    //
-// Design Name:    AXI 4 INTERCONNECT                                            //
-// Module Name:    axi_AW_allocator                                              //
-// Project Name:   PULP                                                          //
-// Language:       SystemVerilog                                                 //
-//                                                                               //
-// Description:   Address Write Allocator: it performs a round robin arbitration //
-//                between pending write requests from each slave port.           //
-//                                                                               //
-// Revision:                                                                     //
-// Revision v0.1 - 01/02/2014 : File Created                                     //
-//                                                                               //
-//                                                                               //
-//                                                                               //
-//                                                                               //
-//                                                                               //
-//                                                                               //
-// ============================================================================= //
+// AW Channel Allocator: Arbitrate AWs between `N_TARG_PORT` slave ports.
+module axi_AW_allocator #(
+  parameter AXI_ADDRESS_W = 32,
+  parameter AXI_USER_W    = 6,
+  parameter N_TARG_PORT   = 7,
+  parameter LOG_N_TARG    = $clog2(N_TARG_PORT),
+  parameter AXI_ID_IN     = 16,
+  parameter AXI_ID_OUT    = AXI_ID_IN + $clog2(N_TARG_PORT)
+) (
+  input  logic                                        clk,
+  input  logic                                        rst_n,
 
-module axi_AW_allocator
-#(
-      parameter  AXI_ADDRESS_W  = 32,
-      parameter  AXI_USER_W     = 6,
-      parameter  N_TARG_PORT    = 7,
-      parameter  LOG_N_TARG     = $clog2(N_TARG_PORT),
-      parameter  AXI_ID_IN      = 16,
-      parameter  AXI_ID_OUT     = AXI_ID_IN + $clog2(N_TARG_PORT)
-)
-(
-      input  logic                                                          clk,
-      input  logic                                                          rst_n,
+  input  logic [N_TARG_PORT-1:0][AXI_ID_IN-1:0]       awid_i,
+  input  logic [N_TARG_PORT-1:0][AXI_ADDRESS_W-1:0]   awaddr_i,
+  input  logic [N_TARG_PORT-1:0][7:0]                 awlen_i,
+  input  logic [N_TARG_PORT-1:0][2:0]                 awsize_i,
+  input  logic [N_TARG_PORT-1:0][1:0]                 awburst_i,
+  input  logic [N_TARG_PORT-1:0]                      awlock_i,
+  input  logic [N_TARG_PORT-1:0][3:0]                 awcache_i,
+  input  logic [N_TARG_PORT-1:0][2:0]                 awprot_i,
+  input  logic [N_TARG_PORT-1:0][3:0]                 awregion_i,
+  input  logic [N_TARG_PORT-1:0][AXI_USER_W-1:0]      awuser_i,
+  input  logic [N_TARG_PORT-1:0][3:0]                 awqos_i,
 
-      //AXI write address bus ----------------------------------------------------------------
-      input  logic [N_TARG_PORT-1:0][AXI_ID_IN-1:0]          awid_i,     //
-      input  logic [N_TARG_PORT-1:0][AXI_ADDRESS_W-1:0]      awaddr_i,   //
-      input  logic [N_TARG_PORT-1:0][ 7:0]                   awlen_i,    //burst length is 1 + (0 - 15)
-      input  logic [N_TARG_PORT-1:0][ 2:0]                   awsize_i,   //size of each transfer in burst
-      input  logic [N_TARG_PORT-1:0][ 1:0]                   awburst_i,  //for bursts>1, accept only incr burst=01
-      input  logic [N_TARG_PORT-1:0]                         awlock_i,   //only normal access supported axs_awlock=00
-      input  logic [N_TARG_PORT-1:0][ 3:0]                   awcache_i,  //
-      input  logic [N_TARG_PORT-1:0][ 2:0]                   awprot_i,   //
-      input  logic [N_TARG_PORT-1:0][ 3:0]                   awregion_i, //
-      input  logic [N_TARG_PORT-1:0][ AXI_USER_W-1:0]        awuser_i,   //
-      input  logic [N_TARG_PORT-1:0][ 3:0]                   awqos_i,    //
-
-      input  logic [N_TARG_PORT-1:0]                         awvalid_i,  //master addr valid
-      output logic [N_TARG_PORT-1:0]                         awready_o,  //slave ready to accept
+  input  logic [N_TARG_PORT-1:0]                      awvalid_i,
+  output logic [N_TARG_PORT-1:0]                      awready_o,
 
 
-      //AXI write address bus--> OUT----------------------------------------------------------------
-      output  logic [AXI_ID_OUT-1:0]                         awid_o,     // Append the Master ID on the MSB bit
-      output  logic [AXI_ADDRESS_W-1:0]                      awaddr_o,   //
-      output  logic [ 7:0]                                   awlen_o,    //burst length is 1 + (0 - 15)
-      output  logic [ 2:0]                                   awsize_o,   //size of each transfer in burst
-      output  logic [ 1:0]                                   awburst_o,  //for bursts>1, accept only incr burst=01
-      output  logic                                          awlock_o,   //only normal access supported axs_awlock=00
-      output  logic [ 3:0]                                   awcache_o,  //
-      output  logic [ 2:0]                                   awprot_o,   //
+  output logic [AXI_ID_OUT-1:0]                       awid_o,
+  output logic [AXI_ADDRESS_W-1:0]                    awaddr_o,
+  output logic [7:0]                                  awlen_o,
+  output logic [2:0]                                  awsize_o,
+  output logic [1:0]                                  awburst_o,
+  output logic                                        awlock_o,
+  output logic [3:0]                                  awcache_o,
+  output logic [2:0]                                  awprot_o,
+  output logic [3:0]                                  awregion_o,
+  output logic [AXI_USER_W-1:0]                       awuser_o,
+  output logic [3:0]                                  awqos_o,
 
-      output  logic [ 3:0]                                   awregion_o, //
-      output  logic [ AXI_USER_W-1:0]                        awuser_o,   //
-      output  logic [ 3:0]                                   awqos_o,    //
+  output logic                                        awvalid_o,
+  input  logic                                        awready_i,
 
-      output logic                                           awvalid_o,  //master addr valid
-      input  logic                                           awready_i,  //slave ready to accept
-
-      // PUSH Interface to DW allocator
-      output logic                                           push_ID_o,
-      output logic [LOG_N_TARG+N_TARG_PORT-1:0]              ID_o, // {BIN_ID, OH_ID};
-      input  logic                                           grant_FIFO_ID_i
+  // Push Interface to DW Allocator
+  output logic                                        push_ID_o,
+  output logic [LOG_N_TARG+N_TARG_PORT-1:0]           ID_o, // {binary ID, one-hot ID};
+  input  logic                                        grant_FIFO_ID_i
 );
 
-localparam      AUX_WIDTH = AXI_ID_IN + AXI_ADDRESS_W + 8 + 3 + 2 + 1 + 4 + 3 +    4 + AXI_USER_W + 4;
+  typedef struct packed {
+    logic [AXI_ID_IN-1:0]     id;
+    logic [AXI_ADDRESS_W-1:0] addr;
+    logic [7:0]               len;
+    logic [2:0]               size;
+    logic [1:0]               burst;
+    logic                     lock;
+    logic [3:0]               cache;
+    logic [2:0]               prot;
+    logic [3:0]               region;
+    logic [AXI_USER_W-1:0]    user;
+    logic [3:0]               qos;
+  } aw_t;
 
+  aw_t [N_TARG_PORT-1:0]  aw_inp;
+  aw_t                    aw_oup;
 
-logic [N_TARG_PORT-1:0][AUX_WIDTH-1:0]                          AUX_VECTOR_IN;
-logic [AUX_WIDTH-1:0]                                           AUX_VECTOR_OUT;
+  logic [N_TARG_PORT-1:0][LOG_N_TARG+N_TARG_PORT-1:0]   id_int;
+  logic [N_TARG_PORT-1:0]                               awready_int;
+  logic                                                 awvalid_int;
 
-logic [N_TARG_PORT-1:0][LOG_N_TARG+N_TARG_PORT-1:0]             ID_in;
-logic [N_TARG_PORT-1:0]                                         awready_int; // Back to processor
-logic                                                           awvalid_int; //to Slave port
+  assign awid_o     = {ID_o[LOG_N_TARG+N_TARG_PORT-1:N_TARG_PORT], aw_oup.id};
+  assign awaddr_o   = aw_oup.addr;
+  assign awlen_o    = aw_oup.len;
+  assign awsize_o   = aw_oup.size;
+  assign awburst_o  = aw_oup.burst;
+  assign awlock_o   = aw_oup.lock;
+  assign awcache_o  = aw_oup.cache;
+  assign awprot_o   = aw_oup.prot;
+  assign awregion_o = aw_oup.region;
+  assign awuser_o   = aw_oup.user;
+  assign awqos_o    = aw_oup.qos;
 
-
-
-genvar i;
-
-
-assign        { awqos_o, awuser_o, awregion_o, awprot_o, awcache_o, awlock_o, awburst_o, awsize_o, awlen_o, awaddr_o, awid_o[AXI_ID_IN-1:0] }  =  AUX_VECTOR_OUT;
-assign        awid_o[AXI_ID_OUT-1:AXI_ID_IN] = ID_o[LOG_N_TARG+N_TARG_PORT-1:N_TARG_PORT];
-
-
-
-assign        awready_o = {N_TARG_PORT{grant_FIFO_ID_i}} & awready_int;
-assign        awvalid_o = awvalid_int & grant_FIFO_ID_i;
-
-assign        push_ID_o = awvalid_o & awready_i & grant_FIFO_ID_i;
-
-generate
-  for(i=0;i<N_TARG_PORT;i++)
-  begin : AUX_VECTOR_BINDING
-      assign AUX_VECTOR_IN[i] =  { awqos_i[i], awuser_i[i], awregion_i[i], awprot_i[i], awcache_i[i], awlock_i[i], awburst_i[i], awsize_i[i], awlen_i[i], awaddr_i[i], awid_i[i]};
+  for (genvar i = 0; i < N_TARG_PORT; i++) begin: gen_aw_inp
+    assign aw_inp[i].id     = awid_i[i];
+    assign aw_inp[i].addr   = awaddr_i[i];
+    assign aw_inp[i].len    = awlen_i[i];
+    assign aw_inp[i].size   = awsize_i[i];
+    assign aw_inp[i].burst  = awburst_i[i];
+    assign aw_inp[i].lock   = awlock_i[i];
+    assign aw_inp[i].cache  = awcache_i[i];
+    assign aw_inp[i].prot   = awprot_i[i];
+    assign aw_inp[i].region = awregion_i[i];
+    assign aw_inp[i].user   = awuser_i[i];
+    assign aw_inp[i].qos    = awqos_i[i];
   end
 
-  for(i=0;i<N_TARG_PORT;i++)
-  begin : ID_VECTOR_BINDING
-      assign ID_in[i][N_TARG_PORT-1:0] =  2**i; // OH ENCODING
-      assign ID_in[i][LOG_N_TARG+N_TARG_PORT-1:N_TARG_PORT] =  i; //BIN ENCODING
-  end
+  assign awready_o  = {N_TARG_PORT{grant_FIFO_ID_i}} & awready_int;
+  assign awvalid_o  = awvalid_int & grant_FIFO_ID_i;
+  assign push_ID_o  = awvalid_o & awready_i & grant_FIFO_ID_i;
 
-endgenerate
+  for (genvar i = 0; i < N_TARG_PORT; i++) begin: gen_id_int
+    assign id_int[i][N_TARG_PORT-1:0] = 2**i;                     // one-hot encoding
+    assign id_int[i][LOG_N_TARG+N_TARG_PORT-1:N_TARG_PORT] = i;   // binary encoding
+  end
 
   axi_node_arbiter #(
-    .AUX_WIDTH  (AUX_WIDTH),
+    .AUX_WIDTH  ($bits(aw_t)),
     .ID_WIDTH   (LOG_N_TARG+N_TARG_PORT),
     .N_MASTER   (N_TARG_PORT)
   ) i_arbiter (
     .clk_i        (clk),
     .rst_ni       (rst_n),
 
-    .inp_id_i     (ID_in),
-    .inp_aux_i    (AUX_VECTOR_IN),
+    .inp_id_i     (id_int),
+    .inp_aux_i    (aw_inp),
     .inp_valid_i  (awvalid_i),
     .inp_ready_o  (awready_int),
 
     .oup_id_o     (ID_o),
-    .oup_aux_o    (AUX_VECTOR_OUT),
+    .oup_aux_o    (aw_oup),
     .oup_valid_o  (awvalid_int),
     .oup_ready_i  (awready_i)
   );
