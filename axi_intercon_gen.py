a07b0fcaa56796c5e51352815e612a0677bdab5b
Olof Kindgren <olof.kindgren@gmail.com>
2019-06-26 12:38:11 +0200

    Support individual id_width in generator

--- axi_intercon_gen.py
+++ axi_intercon_gen.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
+import math
 import sys
 from collections import OrderedDict, defaultdict
 import yaml
 
 from verilogwriter import Signal, Wire, Instance, ModulePort, Port, VerilogWriter
 
-def axi_signals(id_width, addr_width, user_width, data_width):
+class Widths:
+    addr = 0
+    user = 0
+    data = 0
+    max_id = 0
+
+def axi_signals(w, id_width):
     signals = [
         ("awid"    , False, id_width),
-        ("awaddr"  , False, addr_width  ),
+        ("awaddr"  , False, w.addr  ),
         ("awlen"   , False, 8  ),
         ("awsize"  , False, 3  ),
         ("awburst" , False, 2 ),
@@ -16,16 +23,13 @@ def axi_signals(id_width, addr_width, user_width, data_width):
         ("awcache" , False, 4 ),
         ("awprot"  , False, 3 ),
         ("awregion", False, 4),
-    ]
-    if user_width:
-        signals.append(("awuser"  , False, user_width))
-    signals += [
+        ("awuser"  , False, w.user),
         ("awqos"   , False, 4),
         ("awvalid" , False, 0),
         ("awready" , True , 0),
 
         ("arid"    , False, id_width),
-        ("araddr"  , False, addr_width),
+        ("araddr"  , False, w.addr),
         ("arlen"   , False, 8),
         ("arsize"  , False, 3),
         ("arburst" , False, 2),
@@ -33,194 +37,138 @@ def axi_signals(id_width, addr_width, user_width, data_width):
         ("arcache" , False, 4),
         ("arprot"  , False, 3),
         ("arregion", False, 4),
-    ]
-    if user_width:
-        signals.append(("aruser"  , False, user_width))
-    signals += [
+        ("aruser"  , False, w.user),
         ("arqos"   , False, 4),
         ("arvalid" , False, 0),
         ("arready" , True , 0),
             
-        ("wdata" , False, data_width),
-        ("wstrb" , False, data_width//8),
+        ("wdata" , False, w.data),
+        ("wstrb" , False, w.data//8),
         ("wlast" , False, 0),
-    ]
-    if user_width:
-        signals.append(("wuser"  , False, user_width))
-    signals += [
+        ("wuser"  , False, w.user),
         ("wvalid", False, 0),
         ("wready", True , 0),
 
         ("bid"   , True , id_width),
         ("bresp" , True , 2),
         ("bvalid", True , 0),
-    ]
-    if user_width:
-        signals.append(("buser"  , True, user_width))
-    signals += [
+        ("buser"  , True, w.user),
         ("bready", False, 0),
 
         ("rid"   , True , id_width),
-        ("rdata" , True , data_width),
+        ("rdata" , True , w.data),
         ("rresp" , True , 2),
         ("rlast" , True , 0),
-    ]
-    if user_width:
-        signals.append(("ruser"  , True, user_width))
-    signals += [
+        ("ruser"  , True, w.user),
         ("rvalid", True , 0),
         ("rready", False, 0),
     ]
     return signals
 
-class AxiBus(object):
-    def __init__(self, name, id_width, addr_width, user_width, data_width):
-        self.name = name
-        self.signals = [
-            ("awid"    , False, id_width),
-            ("awaddr"  , False, addr_width  ),
-            ("awlen"   , False, 8  ),
-            ("awsize"  , False, 3  ),
-            ("awburst" , False, 2 ),
-            ("awlock"  , False, 0 ),
-            ("awcache" , False, 4 ),
-            ("awprot"  , False, 3 ),
-            ("awregion", False, 4),
-            ]
-        if user_width:
-            self.signals.append(("awuser"  , False, user_width))
-        self.signals += [
-            ("awqos"   , False, 4),
-            ("awvalid" , False, 0),
-            ("awready" , True , 0),
+def module_ports(w, name, id_width, is_input):
+    ports = []
+    for s in axi_signals(w, id_width):
+        if s[0].endswith('user') and not w.user:
+            continue
+        prefix = 'o' if is_input == s[1] else 'i'
+        ports.append(ModulePort("{}_{}_{}".format(prefix, name, s[0]),
+                                'output' if is_input == s[1] else 'input',
+                                s[2]))
+    return ports
 
-            ("arid"    , False, id_width),
-            ("araddr"  , False, addr_width),
-            ("arlen"   , False, 8),
-            ("arsize"  , False, 3),
-            ("arburst" , False, 2),
-            ("arlock"  , False, 0),
-            ("arcache" , False, 4),
-            ("arprot"  , False, 3),
-            ("arregion", False, 4),
-            ]
-        if user_width:
-            self.signals.append(("aruser"  , False, user_width))
-        self.signals += [
-            ("arqos"   , False, 4),
-            ("arvalid" , False, 0),
-            ("arready" , True , 0),
-            
-            ("wdata" , False, data_width),
-            ("wstrb" , False, data_width//8),
-            ("wlast" , False, 0),
-            ]
-        if user_width:
-            self.signals.append(("wuser"  , False, user_width))
-        self.signals += [
-            ("wvalid", False, 0),
-            ("wready", True , 0),
+def assigns(w, max_idw, masters, slaves):
+    raw = '\n'
+    i = 0
+    for m in masters:
+        for s in axi_signals(w, m.idw):
+            if s[0].endswith('user') and not w.user:
+                continue
+            if s[1]:
+                src = "slave_{}[{}]".format(s[0], i)
+                if s[0] in ['bid', 'rid'] and m.idw < max_idw:
+                    src = src+'[{}:0]'.format(m.idw-1)
+                raw += "   assign o_{}_{} = {};\n".format(m.name, s[0], src)
+            else:
+                src = "i_{}_{}".format(m.name, s[0])
+                if s[0] in ['arid', 'awid'] and m.idw < max_idw:
+                    src = "{"+ str(max_idw-m.idw)+"'d0,"+src+"}"
+                raw += "   assign slave_{}[{}] = {};\n".format(s[0], i, src)
+        raw += "   assign connectivity_map[{}] = {}'b{};\n".format(i, len(slaves), '1'*len(slaves))
+        i += 1
 
-            ("bid"   , True , id_width),
-            ("bresp" , True , 2),
-            ("bvalid", True , 0),
-            ]
-        if user_width:
-            self.signals.append(("buser"  , True, user_width))
-        self.signals += [
-            ("bready", False, 0),
+    raw += '\n'
 
-            ("rid"   , True , id_width),
-            ("rdata" , True , data_width),
-            ("rresp" , True , 2),
-            ("rlast" , True , 0),
-            ]
-        if user_width:
-            self.signals.append(("ruser"  , True, user_width))
-        self.signals += [
-            ("rvalid", True , 0),
-            ("rready", False, 0),
-        ]
+    i = 0
+    for m in slaves:
+        for s in axi_signals(w, max_idw):
+            if s[0].endswith('user') and not w.user:
+                continue
+            if s[1]:
+                raw += "   assign master_{}[{}] = i_{}_{};\n".format(s[0], i, m.name, s[0])
+            else:
+                raw += "   assign o_{}_{} = master_{}[{}];\n".format(m.name, s[0], s[0], i)
+        raw += "   assign start_addr[0][{}] = 32'h{:08x};\n".format(i, m.offset)
+        raw += "   assign end_addr[0][{}] = 32'h{:08x};\n".format(i, m.offset+m.size-1)
+        i += 1
+    raw += "   assign valid_rule[0] = {}'b{};\n".format(len(slaves), '1'*len(slaves))
+    return raw
 
-    def assigns(self, masters, slaves, addr_width):
-        raw = '\n'
-        i = 0
-        for m in masters:
-            for s in self.signals:
-                if s[1]:
-                    raw += "   assign o_{}_{} = slave_{}[{}];\n".format(m.name, s[0], s[0], i)
-                else:
-                    raw += "   assign slave_{}[{}] = i_{}_{};\n".format(s[0], i, m.name, s[0])
-            raw += "   assign connectivity_map[{}] = {}'b{};\n".format(i, len(slaves), '1'*len(slaves))
-            i += 1
-
-        raw += '\n'
-
-        i = 0
-        for m in slaves:
-            for s in self.signals:
-                if s[1]:
-                    raw += "   assign master_{}[{}] = i_{}_{};\n".format(s[0], i, m.name, s[0])
-                else:
-                    raw += "   assign o_{}_{} = master_{}[{}];\n".format(m.name, s[0], s[0], i)
-            raw += "   assign start_addr[0][{}] = 32'h{:08x};\n".format(i, m.offset)
-            raw += "   assign end_addr[0][{}] = 32'h{:08x};\n".format(i, m.offset+m.size-1)
-            i += 1
-        raw += "   assign valid_rule[0] = {}'b{};\n".format(len(slaves), '1'*len(slaves))
-        return raw
-
-    def module_ports(self, is_input):
-        ports = []
-        for s in self.signals:
-            prefix = 'o' if is_input == s[1] else 'i'
-            ports.append(ModulePort("{}_{}_{}".format(prefix, self.name, s[0]),
-                                    'output' if is_input == s[1] else 'input',
-                                    s[2]))
-        return ports
-
-    def instance_ports(self, masters, slaves):
-        ports = [Port('clk'  , 'clk'),
-                 Port('rst_n', 'rst_n'),
-                 Port('test_en_i', "1'b0")]
-        for s in self.signals:
-            suffix = 'o' if s[1] else 'i'
-            name = "slave_{}_{}".format(s[0], suffix)
+def instance_ports(w, id_width, masters, slaves):
+    ports = [Port('clk'  , 'clk'),
+             Port('rst_n', 'rst_n'),
+             Port('test_en_i', "1'b0"),
+             Port('slave_awatop_i', "{}'d0".format(len(masters)*6)),
+             Port('master_awatop_o', '')]
+    for s in axi_signals(w, id_width):
+        suffix = 'o' if s[1] else 'i'
+        name = "slave_{}_{}".format(s[0], suffix)
+        if s[0].endswith('user') and not w.user:
+            value = "" if s[1] else "{}'d0".format(len(masters))
+        else:
             value = "slave_{}".format(s[0])
-            ports.append(Port(name, value))
+        ports.append(Port(name, value))
 
-        for s in self.signals:
-            suffix = 'i' if s[1] else 'o'
-            name = "master_{}_{}".format(s[0], suffix)
+    for s in axi_signals(w, id_width):
+        suffix = 'i' if s[1] else 'o'
+        name = "master_{}_{}".format(s[0], suffix)
+        if s[0].endswith('user') and not w.user:
+            value = "{}'d0".format(len(slaves)) if s[1] else ""
+        else:
             value = "master_{}".format(s[0])
-            ports.append(Port(name, value))
+        ports.append(Port(name, value))
 
-        value = '{' + ', '.join(["32'h{start:08x}".format(start=s.offset) for s in slaves]) + '}'
-        ports.append(Port('cfg_START_ADDR_i', 'start_addr'))#value))
+    value = '{' + ', '.join(["32'h{start:08x}".format(start=s.offset) for s in slaves]) + '}'
+    ports.append(Port('cfg_START_ADDR_i', 'start_addr'))#value))
 
-        value = '{' + ', '.join(["32'h{end:08x}".format(end=s.offset+s.size-1) for s in slaves]) + '}'
-        ports.append(Port('cfg_END_ADDR_i', 'end_addr'))#value))
-        ports.append(Port('cfg_valid_rule_i', "valid_rule"))
-        ports.append(Port('cfg_connectivity_map_i', "connectivity_map"))
-        return ports
+    value = '{' + ', '.join(["32'h{end:08x}".format(end=s.offset+s.size-1) for s in slaves]) + '}'
+    ports.append(Port('cfg_END_ADDR_i', 'end_addr'))#value))
+    ports.append(Port('cfg_valid_rule_i', "valid_rule"))
+    ports.append(Port('cfg_connectivity_map_i', "connectivity_map"))
+    return ports
 
-    def template_ports(self, is_input):
-        ports = []
-        for s in self.signals:
-            port_name = "{}_{}".format(self.name, s[0])
-            prefix = 'o' if is_input == s[1] else 'i'
-            ports.append(Port("{}_{}".format(prefix, port_name), port_name))
-        return ports
+def template_ports(w, name, id_width, is_input):
+    ports = []
+    for s in axi_signals(w, id_width):
+        if s[0].endswith('user') and not w.user:
+            continue
+        port_name = "{}_{}".format(name, s[0])
+        prefix = 'o' if is_input == s[1] else 'i'
+        ports.append(Port("{}_{}".format(prefix, port_name), port_name))
+    return ports
 
-    def template_wires(self):
-        wires = []
-        for s in self.signals:
-            wires.append(Wire("{}_{}".format(self.name, s[0]), s[2]))
-        return wires
+def template_wires(w, name, id_width):
+    wires = []
+    for s in axi_signals(w, id_width):
+        if s[0].endswith('user') and not w.user:
+            continue
+        wires.append(Wire("{}_{}".format(name, s[0]), s[2]))
+    return wires
 
 class Master:
     def __init__(self, name, d=None):
         self.name = name
         self.slaves = []
+        self.idw = 1
         if d:
             self.load_dict(d)
 
@@ -229,6 +177,8 @@ class Master:
             if key == 'slaves':
                 # Handled in file loading, ignore here
                 continue
+            if key == 'id_width':
+                self.idw = value
             else:
                 raise UnknownPropertyError(
                     "Unknown property '%s' in master section '%s'" % (
@@ -321,46 +271,50 @@ class AxiIntercon:
                 print(' ' + master.name)
                             
     def write(self):
-        #FIXME
-        addr_width = 32
-        data_width = 64
-        id_width = 3
-        user_width = 1
+        w = Widths()
+        w.addr = 32
+        w.data = 64
+        w.user = 0
+
+        max_idw = max([m.idw for m in self.masters])
+        max_sidw = max_idw + math.ceil(math.log2(len(self.masters)))
         file = self.output_file
 
-        template_ports = [Port('clk'  , 'clk'),
-                          Port('rst_n', 'rstn')]
+        _template_ports = [Port('clk'  , 'clk'),
+                           Port('rst_n', 'rstn')]
         template_parameters = []
 
         #Module header
         self.verilog_writer.add(ModulePort('clk'  , 'input'))
         self.verilog_writer.add(ModulePort('rst_n', 'input'))
         for master in self.masters:
-            bus = AxiBus(master.name, id_width, addr_width, user_width, data_width)
-            for port in bus.module_ports(True):
+            for port in module_ports(w, master.name, master.idw, True):
                 self.verilog_writer.add(port)
-            for wire in bus.template_wires():
+            for wire in template_wires(w, master.name, master.idw):
                 self.template_writer.add(wire)
-            template_ports += bus.template_ports(True)
+            _template_ports += template_ports(w, master.name, master.idw, True)
 
         for slave in self.slaves:
-            bus = AxiBus(slave.name, id_width+1, addr_width, user_width, data_width)
-            for port in bus.module_ports(False):
+            for port in module_ports(w, slave.name, max_sidw, False):
                 self.verilog_writer.add(port)
-            for wire in bus.template_wires():
+            for wire in template_wires(w, slave.name, max_sidw):
                 self.template_writer.add(wire)
-            template_ports += bus.template_ports(False)
+            _template_ports += template_ports(w, slave.name, max_sidw, False)
 
         raw = ""
 
         nm = len(self.masters)
-        for s in axi_signals(id_width, addr_width, user_width, data_width):
+        for s in axi_signals(w, max_idw):
+            if s[0].endswith('user') and not w.user:
+                continue
             raw += "   wire [{}:0]".format(nm-1)
             if s[2]:
                 raw += "[{}:0]".format(s[2]-1)
             raw += " slave_{};\n".format(s[0])
         ns = len(self.slaves)
-        for s in axi_signals(id_width+1, addr_width, user_width, data_width):
+        for s in axi_signals(w, max_sidw):
+            if s[0].endswith('user') and not w.user:
+                continue
             raw += "   wire [{}:0]".format(ns-1)
             if s[2]:
                 raw += "[{}:0]".format(s[2]-1)
@@ -371,20 +325,20 @@ class AxiIntercon:
    wire [0:0][{ns}:0][{aw}:0] end_addr;
    wire [0:0][{ns}:0]       valid_rule;
    wire [{nm}:0][{ns}:0]      connectivity_map;
-        """.format(nm=nm-1,aw=addr_width-1, ns=ns-1)
+        """.format(nm=nm-1,aw=w.addr-1, ns=ns-1)
             
-        raw += AxiBus("", id_width, addr_width, user_width, data_width).assigns(self.masters, self.slaves, addr_width)
+        raw += assigns(w, max_idw, self.masters, self.slaves)
 
         self.verilog_writer.raw = raw
-        parameters = [Parameter('AXI_ADDRESS_W', addr_width),
-                      Parameter('AXI_DATA_W'   , data_width),
+        parameters = [Parameter('AXI_ADDRESS_W', w.addr),
+                      Parameter('AXI_DATA_W'   , w.data),
                       Parameter('N_MASTER_PORT', len(self.slaves)),
                       Parameter('N_SLAVE_PORT' , len(self.masters)),
-                      Parameter('AXI_ID_IN'    , id_width),
-                      Parameter('AXI_USER_W'   , user_width),
+                      Parameter('AXI_ID_IN'    , max_idw),
+                      Parameter('AXI_USER_W'   , w.user or 1),
                       Parameter('N_REGION'     , 1),
                       ]
-        ports = AxiBus(name, id_width, addr_width, user_width, data_width).instance_ports(self.masters, self.slaves)
+        ports = instance_ports(w, max_idw, self.masters, self.slaves)
         
         self.verilog_writer.add(Instance('axi_node',
                                          'axi_node',
@@ -394,7 +348,7 @@ class AxiIntercon:
         self.template_writer.add(Instance(self.name,
                                           self.name,
                                           template_parameters,
-                                          template_ports))
+                                          _template_ports))
 
         self.verilog_writer.write(file)
         self.template_writer.write(file+'h')
